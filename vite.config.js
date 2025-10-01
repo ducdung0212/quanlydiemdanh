db32cdf67760a169e1221d5f7da462ab0f9164ae
ducdung0212 <buihoangducdung04@gmail.com>
2025-10-01 23:27:07 +0700

    Add files via upload
    Lam mau giao dien Dashboard

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,5 @@
-﻿import { defineConfig } from 'vite';
+// vite.config.js
+import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
@@ -12,3 +13,4 @@ export default defineConfig({
     }),
   ],
 });
+
