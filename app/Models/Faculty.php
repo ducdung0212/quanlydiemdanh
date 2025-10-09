362a63b9a0da9c3c98022761db5384ee0433d3a4
Dũng Bùi <buihoangducdung04@gmailcom>
2025-10-09 16:38:33 +0700

    Sua lai giao dien user

---
+++ app/Models/Faculty.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Faculty extends Model
+{
+    use HasFactory;
+
+    protected $table = 'faculties';
+    protected $primaryKey = 'code';
+    public $incrementing = false; // vì khóa chính không phải auto increment
+    protected $keyType = 'string';
+
+    protected $fillable = [
+        'code',
+        'name',
+    ];
+
+    /* ========================
+     |   ĐỊNH NGHĨA QUAN HỆ
+     ======================== */
+
+    // Một khoa có nhiều lớp
+    public function classes()
+    {
+        return $this->hasMany(ClassModel::class, 'faculty_code', 'code');
+    }
+
+    // Một khoa có nhiều giảng viên
+    public function lecturers()
+    {
+        return $this->hasMany(Lecturer::class, 'faculty_code', 'code');
+    }
+}
